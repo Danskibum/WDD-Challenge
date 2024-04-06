169bc4e759d7a6825dd0f003a39c82d39578111e
DANIEL GERDES <ger19007@byui.edu>
2024-04-05 21:55:45 -0700

    have not uploaded in a while

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,17 @@
-import { loadHeaderFooter } from "./utils.mjs";
+window.onload = function() {
+    const directory ="src/images/animals/";
 
-loadHeaderFooter();
+    fetch(directory)
+        .then(response => response.text())
+        .then(text =>{
+            const files = text.split("/n").filter(file =>file.trim() !== "");
+            const gallery = document.getElementById("gallery");
+            files.forEach(file => {
+                const img = document.createElement("img");
+                img.src = directory + file;
+                img.alt = file; 
+            gallery.appendChild(img);
+            }); 
+        })
+        .catch(error => console.error("Error fetching images:", error));
+}
